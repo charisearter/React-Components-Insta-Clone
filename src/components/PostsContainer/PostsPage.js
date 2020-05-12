3d1bc452111384bb0170652034b3f4dbaf430579
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-12 17:56:14 -0500

    posts done

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
 
+// import data 
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [ posts, setPosts] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-    
       {/* map through data here to return a Post and pass data as props to Post */}
+     { posts.map = ((index, i)=> {
+       //if warning appears, add ( after index, i ... after Post add key={i}  to remove warning))
+        return <Post key = {i} posts = {index} />
+      })}
     </div>
   );
 };
